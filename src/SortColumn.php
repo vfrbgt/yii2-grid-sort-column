fcbfb68d1228e36c3de3d993e7cb8de649607d1e
Lipovtsev Bogdan <seclan@mail.ru>
2015-12-10 03:54:21 +0700

    first commit

--- src/SortColumn.php
+++ src/SortColumn.php
@@ -12,6 +12,7 @@ class SortColumn extends Column
 {
     public $controller;
     public $urlCreator;
+    public $view;
 
 
     /**
@@ -20,6 +21,30 @@ class SortColumn extends Column
     public function init()
     {
         parent::init();
+        $view = $this->grid->getView();
+        $script = '$(document).ready(function(){
+                $(\'.sort-up\').on(\'click\', function(){
+                    var element = $(this).parent().parent();
+                    var id1 = element.attr(\'data-key\');
+                    var id2 = element.prev().attr(\'data-key\');
+                    element.prev().before(element);
+                    $.ajax({
+                        url: "'.Url::toRoute(['swap']).'?idFrom="+id1+"&idTo="+id2
+                    });
+                    return false;
+                });
+                $(\'.sort-down\').on(\'click\' ,function(){
+                    var element = $(this).parent().parent();
+                    var id1 = element.attr(\'data-key\');
+                    var id2 = element.next().attr(\'data-key\');
+                    element.next().after(element);
+                    $.ajax({
+                        url: "'.Url::toRoute(['swap']).'?idFrom="+id1+"&idTo="+id2
+                    });
+                    return false;
+                });
+            });';
+        $view->registerJs($script);
     }
 
     /**
@@ -29,32 +54,5 @@ class SortColumn extends Column
     {
         return '<a href="" class="sort-up" data-id="'.$key.'"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a>
                 <a href="" class="sort-down" data-id="'.$key.'"><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></a>';
-    }
-
-    protected function renderHeaderCellContent() {
-        return '<script>
-                    $(document).ready(function(){
-                        $(\'.sort-up\').on(\'click\', function(){
-                            var element = $(this).parent().parent();
-                            var id1 = element.attr(\'data-key\');
-                            var id2 = element.prev().attr(\'data-key\');
-                            element.prev().before(element);
-                            $.ajax({
-                                url: "'.Url::toRoute(['swap']).'?idFrom="+id1+"&idTo="+id2
-                            });
-                            return false;
-                        });
-                        $(\'.sort-down\').on(\'click\' ,function(){
-                            var element = $(this).parent().parent();
-                            var id1 = element.attr(\'data-key\');
-                            var id2 = element.next().attr(\'data-key\');
-                            element.next().after(element);
-                            $.ajax({
-                                url: "'.Url::toRoute(['swap']).'?idFrom="+id1+"&idTo="+id2
-                            });
-                            return false;
-                        });
-                    });
-                </script>';
     }
 }
