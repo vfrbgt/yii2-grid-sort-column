a41ad800afee9a1be7a3a94c346645617539aaf9
Lipovtsev Bogdan <seclan@mail.ru>
2015-12-10 05:06:31 +0700

    first commit

---
+++ src/SortBehavior.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\gridSortColumn;
+
+use yii;
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+
+class Slug extends Behavior
+{
+    public $attribute;
+    public $model;
+
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'attributeInc'
+        ];
+    }
+
+    public function attributeInc($event) {
+        $maxValue = $this->owner->find()->max($this->attribute);
+        $this->owner->{$this->attribute} = $maxValue+1;
+    }
+}
